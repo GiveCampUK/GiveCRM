ee0c0a8e3a843631690acaf35e0c44acbd7f39ba
AnthonySteele <Anthony@PlasticAvalon.org>
2011-10-21 21:47:46 +0100

    Id not ID

--- src/GiveCRM.Models/Member.cs
+++ src/GiveCRM.Models/Member.cs
@@ -4,7 +4,7 @@ namespace GiveCRM.Models
 {
     public class Member
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
--- src/GiveCRM.Models/PhoneNumber.cs
+++ src/GiveCRM.Models/PhoneNumber.cs
@@ -2,8 +2,8 @@ namespace GiveCRM.Models
 {
     public class PhoneNumber
     {
-        public int ID { get; set; }
-        public int MemberID { get; set; }
+        public int Id { get; set; }
+        public int MemberId { get; set; }
         public string Type { get; set; }
         public string Number { get; set; }
     }
