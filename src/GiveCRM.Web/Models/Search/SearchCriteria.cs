9aeb5680ebae46f3a0dcbf451fa02df0931b34c1
Saqib Shaikh <Me@SaqibShaikh.com>
2011-10-22 12:19:19 +0100

    Initial work on querying/searching

---
+++ src/GiveCRM.Web/Models/Search/SearchCriteria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiveCRM.Web.Models.Search
+{
+    public enum SearchOperator
+    {
+        EqualTo,
+        NotEqualTo,
+        LessThan,
+        LessThanOrEqualTo,
+        GreaterThan,
+        GreaterThanOrEqualTo,
+        StartsWith,
+        EndsWith,
+        Contains,
+        AnyOf,
+        AllOf
+    }
+
+    public enum SearchFieldType
+    {
+        Text,
+        Number,
+        Boolean,
+        Date,
+        SelectList
+    }
+
+    public class SearchCriteria
+    {
+        public string Field { get; set; }
+        public SearchFieldType Type { get; set; }
+        public SearchOperator Operator { get; set; }
+        public string Value { get; set; }
+        public string DatabaseTableName { get; set; }
+        public string DatabaseColumnName { get; set; }
+    }
+}
