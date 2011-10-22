6cbf615d5c35bf86f00a9c9bc0e0abfc773f1453
AnthonySteele <Anthony@PlasticAvalon.org>
2011-10-22 20:37:35 +0100

    Formatting

--- src/GiveCRM.Web/Views/MenuHelper.cs
+++ src/GiveCRM.Web/Views/MenuHelper.cs
@@ -1,34 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Mvc.Html;
 
-namespace GiveCRM.Web.Views {
-	public static class MenuHelper {
+namespace GiveCRM.Web.Views 
+{
+	public static class MenuHelper 
+    {
 
-	public static bool IsCurrentPage(
-		this HtmlHelper htmlHelper,
-		string controllerName,
-		string actionName = null)
-	{
-		var routeData = htmlHelper.ViewContext.RouteData;
-		string currentAction = routeData.GetRequiredString("action");
-		string currentController = routeData.GetRequiredString("controller");
+	    public static bool IsCurrentPage(
+		    this HtmlHelper htmlHelper,
+		    string controllerName,
+		    string actionName = "")
+	    {
+		    var routeData = htmlHelper.ViewContext.RouteData;
+		    string currentAction = routeData.GetRequiredString("action");
+		    string currentController = routeData.GetRequiredString("controller");
 
-		if (string.IsNullOrEmpty(actionName))
-		{
-			actionName = currentAction;
-		}
+		    if (string.IsNullOrEmpty(actionName))
+		    {
+			    actionName = currentAction;
+		    }
 
-		return (controllerName == currentController && actionName == currentAction);
-	}
+		    return (controllerName == currentController && actionName == currentAction);
+	    }
 
 	public static MvcHtmlString NavigationClassBuilder(
 		this HtmlHelper htmlHelper,
 		string controllerName,
-		string actionName = null,
+		string actionName = "",
 		string additionalClasses = "")
 	{
 		List<string> classList = additionalClasses.Split(' ').ToList(); 
@@ -37,8 +36,9 @@ namespace GiveCRM.Web.Views {
 			classList.Add("active");
 		}
 		
-		if (classList.Count == 0) {
-		 return new MvcHtmlString("");
+		if (classList.Count == 0) 
+        {
+            return new MvcHtmlString("");
 		}
 
 		return new MvcHtmlString("class=\"" + string.Join(" ", classList.ToArray()) + "\"");
