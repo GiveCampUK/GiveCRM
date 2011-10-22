a63f70282a12c9ac5e24367aac3095f4f4407a6a
AnthonySteele <Anthony@PlasticAvalon.org>
2011-10-22 11:13:14 +0100

    added dummy data generator project

---
+++ src/GiveCRM.DummyDataGenerator/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace GiveCRM.DummyDataGenerator
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        private readonly Generator generator = new Generator();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateMembers(object sender, RoutedEventArgs e)
+        {
+            generationOutput.Text = generator.GenerateMembers();
+        }
+
+        private void GenerateCampaign(object sender, RoutedEventArgs e)
+        {
+            generationOutput.Text = generator.GenerateCampaign();
+        }
+
+        private void GenerateDonations(object sender, RoutedEventArgs e)
+        {
+            generationOutput.Text = generator.GenerateDonations();
+
+        }
+    }
+}
