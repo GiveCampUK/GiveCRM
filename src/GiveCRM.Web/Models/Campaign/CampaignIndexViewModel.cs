49eae083c2bbc7069cc07913de9f80cc9f7944b2
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2011-10-22 12:25:00 +0100

    CoreUI: Built empty Campaign Create UI

--- src/GiveCRM.Web/Models/Campaign/CampaignIndexViewModel.cs
+++ src/GiveCRM.Web/Models/Campaign/CampaignIndexViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using GiveCRM.Models;
 
 namespace GiveCRM.Web.Models.Campaign
 {
-    public class CampaignIndexViewModel
+    public class CampaignIndexViewModel : ViewModelBase
     {
-        public string Title { get; set; }
+        public CampaignIndexViewModel(string title) : base(title)
+        {
+        }
+
         public bool ShowClosed { get; set; }
         public string ShowCampaignsLinkText { get; set; }
+        public string CreateCampaignLinkText { get; set; }
         public IEnumerable<GiveCRM.Models.Campaign> Campaigns { get; set; }
     }
 }
