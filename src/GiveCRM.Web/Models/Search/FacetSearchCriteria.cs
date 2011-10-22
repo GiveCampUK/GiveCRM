32f454059e1fea74a1e5097321bfe2ca91509701
Saqib Shaikh <Me@SaqibShaikh.com>
2011-10-22 19:31:13 +0100

    More search criteria

---
+++ src/GiveCRM.Web/Models/Search/FacetSearchCriteria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GiveCRM.Models;
+using GiveCRM.DataAccess;
+
+namespace GiveCRM.Web.Models.Search
+{
+    public class FacetSearchCriteria : SearchCriteria
+    {
+        public static IEnumerable<SearchCriteria> GetEmptyCriteria()
+        {
+            return null;
+        }
+
+        public override bool IsMatch(Member m)
+        {
+            switch (this.InternalName)
+            {
+                default: return false;
+            }
+        }
+    }
+}
