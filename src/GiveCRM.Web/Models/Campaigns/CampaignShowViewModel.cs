1ed73e8f4a4e8aef8afcd3ba7a7d09fe3fa08887
Adrian Banks <github@adrianbanks.co.uk>
2011-10-22 15:23:36 +0100

    rename the Campaign namespace to Campaigns

--- src/GiveCRM.Web/Models/Campaigns/CampaignShowViewModel.cs
+++ src/GiveCRM.Web/Models/Campaigns/CampaignShowViewModel.cs
@@ -1,9 +1,12 @@
-﻿namespace GiveCRM.Web.Models.Campaign
+﻿using GiveCRM.Models;
+
+namespace GiveCRM.Web.Models.Campaigns
 {
     public class CampaignShowViewModel : ViewModelBase
     {
+        public Campaign Campaign { get; set; }
+
         public CampaignShowViewModel(string title) : base(title)
-        {
-        }
+        {}
     }
 }
