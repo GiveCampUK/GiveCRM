70f531695a9add93ee78f64bdb3a178d6d8cd9dd
AnthonySteele <Anthony@PlasticAvalon.org>
2011-10-22 11:13:14 +0100

    added dummy data generator project

---
+++ src/GiveCRM.DummyDataGenerator/Generation/RandomSource.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace GiveCRM.DummyDataGenerator.Generation
+{
+    using System.Collections.Generic;
+
+    internal class RandomSource
+    {
+        private readonly Random random = new Random();
+
+
+        public int Next(int max)
+        {
+            return random.Next(max);
+        }
+
+        public string PhoneDigits()
+        {
+            string prefix = "0" + random.Next(100) + " ";
+            return prefix + random.Next(10000).ToString("0000") + random.Next(10000).ToString("0000");
+        }
+
+        public T PickFromList<T>(List<T> list)
+        {
+            var max = list.Count - 1;
+            var index = random.Next(max);
+            return list[index];
+        }
+
+        public bool Percent(int passPercent)
+        {
+            var value = random.Next(100);
+            return value < passPercent;
+        }
+
+        public string Letter()
+        {
+            var offset = random.Next(25);
+            var index = (int)'A' + offset;
+
+            var letter = (char)index;
+
+            return new string(new[] { letter });
+        }
+
+        public bool Bool()
+        {
+            var value = random.Next(100);
+            return value < 50;
+        }
+    }
+}
---
+++ src/GiveCRM.DummyDataGenerator/Generator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace GiveCRM.DummyDataGenerator
+{
+    using GiveCRM.DataAccess;
+    using GiveCRM.DummyDataGenerator.Generation;
+    using GiveCRM.Models;
+
+    internal class Generator
+    {
+        private Campaign campaign;
+        private List<Member> members;
+
+        internal string GenerateMembers()
+        {
+            MemberGenerator generator = new MemberGenerator();
+            members = generator.Generate(1000);
+
+            var membersDb = new Members();
+            foreach (var member in members)
+            {
+                membersDb.Insert(member);
+            }
+
+            return string.Format("{0} members saved", members.Count);
+        }
+
+        internal string GenerateCampaign()
+        {
+            CampaignGenerator generator = new CampaignGenerator();
+            campaign = generator.Generate();
+
+            return "Generated campaign " + campaign;
+        }
+
+        internal string GenerateDonations()
+        {
+            DonationsGenerator generator = new DonationsGenerator(campaign, members);
+            var donations = generator.Generate();
+
+            return string.Format("{0} donations generated", donations.Count);
+        }
+    }
+}
