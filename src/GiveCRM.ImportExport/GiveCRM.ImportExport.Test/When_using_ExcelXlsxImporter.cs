3aac81d37b39c2eee391d999106bc5958ec7d69c
johanbarnardtm <johanbarnardtm@yahoo.co.uk>
2011-10-22 09:35:11 +0100

    Added Excel adapters
    added test data files
    Added EPlus package

---
+++ src/GiveCRM.ImportExport/GiveCRM.ImportExport.Test/When_using_ExcelXlsxImporter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GiveCRM.ImportExport.Test
+{
+    [TestFixture]
+    public class When_using_ExcelXlsxImporter
+    {
+        private string _testFileXlsx = Directory.GetCurrentDirectory() + "/TestData/MemberData.xlsx";
+
+        [Test]
+        public void Should_throw_exception_if_file_stream_null()
+        {
+            var importer = new ExcelXlsxImporter();
+
+            Assert.Throws<ArgumentNullException>(() => importer.Open(null));
+        }
+
+        [Test]
+        public void Should_set_package_from_stream()
+        {
+            using (FileStream stream = new FileStream(_testFileXlsx, FileMode.Open, FileAccess.Read))
+            {
+                var importer = new ExcelXlsxImporter();
+                importer.Open(stream);
+
+                Assert.IsNotNull(importer.Package);
+            }
+        }
+
+        [Test]
+        public void Should_set_workbook_from_stream()
+        {
+            using (FileStream stream = new FileStream(_testFileXlsx, FileMode.Open, FileAccess.Read))
+            {
+                var importer = new ExcelXlsxImporter();
+                importer.Open(stream);
+
+                Assert.IsNotNull(importer.Workbook);
+            }
+        }
+    }
+}
---
+++ src/GiveCRM.ImportExport/GiveCRM.ImportExport/ExcelXlsxImporter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml;
+using Cell = GiveCRM.ImportExport.Cells.Cell;
+
+namespace GiveCRM.ImportExport
+{
+    public class ExcelXlsxImporter : IExcelImporter
+    {
+        internal ExcelWorkbook Workbook;
+        internal ExcelPackage Package;
+
+        public void Open(Stream stream)
+        {
+            if (stream == null) throw new ArgumentNullException("stream");
+            Package = new ExcelPackage(stream);
+            Workbook = Package.Workbook;
+        }
+
+        public IEnumerable<IEnumerable<Cell>> GetRows(int sheetIndex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
