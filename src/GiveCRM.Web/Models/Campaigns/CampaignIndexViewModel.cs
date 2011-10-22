d5c1f715725c678f04dd4dfa62948add0df3d3e2
Adrian Banks <github@adrianbanks.co.uk>
2011-10-22 15:23:36 +0100

    rename the Campaign namespace to Campaigns

--- src/GiveCRM.Web/Models/Campaigns/CampaignIndexViewModel.cs
+++ src/GiveCRM.Web/Models/Campaigns/CampaignIndexViewModel.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GiveCRM.Models;
 
-namespace GiveCRM.Web.Models.Campaign
+namespace GiveCRM.Web.Models.Campaigns
 {
     public class CampaignIndexViewModel : ViewModelBase
     {
         public CampaignIndexViewModel(string title) : base(title)
-        {
-        }
+        {}
 
         public bool ShowClosed { get; set; }
         public string ShowCampaignsLinkText { get; set; }
         public string CreateCampaignLinkText { get; set; }
-        public IEnumerable<GiveCRM.Models.Campaign> Campaigns { get; set; }
+        public IEnumerable<Campaign> Campaigns { get; set; }
     }
 }
