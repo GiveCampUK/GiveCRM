48f947efd8246d380b4f66e39eca8769bec801e8
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2011-10-22 21:33:31 +0100

    Implemented CampaignController.DownloadMailingList()

--- src/GiveCRM.Web/Services/MailingListService.cs
+++ src/GiveCRM.Web/Services/MailingListService.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using GiveCRM.ImportExport;
 using GiveCRM.Models;
 
 namespace GiveCRM.Web.Services
 {
     public class MailingListService : IMailingListService
     {
+        private readonly IExcelExport _excelExport;
+
+        public MailingListService(IExcelExport excelExport)
+        {
+            _excelExport = excelExport;
+        }
+
         public void WriteToStream(IEnumerable<Member> members, Stream stream, OutputFormat outputFormat)
         {
             throw new NotImplementedException();
+
+            // Build excel graph of cells
+            // Ask excel exporter to write it to the stream
         }
     }
 }
