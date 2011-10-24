3ff4c668cc4e9c493b0348928e36ea558215c5f4
Paul Stack <paul@paulstack.co.uk>
2011-10-24 14:40:50 +0100

    removed the dependency of the DonationsDb from the Donations controller - this is now in a Donations ServiceLayer class

---
+++ src/GiveCRM.Web/Services/IDonationsService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using GiveCRM.Models;
+
+namespace GiveCRM.Web.Services
+{
+    public interface IDonationsService
+    {
+        IEnumerable<Donation> GetTopDonations();
+        IEnumerable<Donation> GetLatestDonations();
+        void QuickDonation(Donation donation);
+    }
+}
