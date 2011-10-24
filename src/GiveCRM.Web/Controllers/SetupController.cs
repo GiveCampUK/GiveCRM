791b266b428a56be4e9161cf8a3fcdec6ad82c27
Paul Stack <paul@paulstack.co.uk>
2011-10-24 15:06:18 +0100

    removal of the using GiveCRM.DataAccess from SetUp controller as all references to DataAccess now hidden

--- src/GiveCRM.Web/Controllers/SetupController.cs
+++ src/GiveCRM.Web/Controllers/SetupController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using GiveCRM.DataAccess;
 using GiveCRM.Models;
 using GiveCRM.Web.Models.Facets;
 using GiveCRM.Web.Services;
