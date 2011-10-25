b776294db670fd63f6aa5caf2102072f70a7e635
Paul Stack <paul@paulstack.co.uk>
2011-10-25 12:37:30 +0100

    addition of a class library to start testing the controller functionality - this will give us a way to separate out the functionality and make it more SOLID

---
+++ src/GiveCRM.Web.Tests/controllers/HomeController.Tests.cs
@@ -0,0 +1,28 @@
+﻿using GiveCRM.Web.Controllers;
+using MvcContrib.TestHelper;
+using NUnit.Framework;
+
+namespace GiveCRM.Web.Tests.controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Home_Action_Returns_View_When_Called()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+
+            result.AssertViewRendered();
+        }
+
+        [Test]
+        public void About_Action_Returns_View_When_Called()
+        {
+            var controller = new HomeController();
+            var result = controller.About();
+
+            result.AssertViewRendered();
+        }
+    }
+}
