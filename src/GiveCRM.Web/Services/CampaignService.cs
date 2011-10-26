da473255d387058ac993d8aff09f677de42a725b
Paul Stack <paul@paulstack.co.uk>
2011-10-26 15:42:36 +0100

    separation of interfaces from the corresponding classes to follow in place convention, also remove of the last of the dependencies of the DataAccess layer from the Campaign controller, this included the creation of MemberSearchFilterService layer

--- src/GiveCRM.Web/Services/CampaignService.cs
+++ src/GiveCRM.Web/Services/CampaignService.cs
@@ -34,13 +34,4 @@ namespace GiveCRM.Web.Services
         }
 
     }
-
-    public interface ICampaignService
-    {
-        IEnumerable<Campaign> AllOpen();
-        IEnumerable<Campaign> AllClosed();
-        Campaign Get(int id);
-        Campaign Insert(Campaign campaign);
-        void Update(Campaign campaign);
-    }
 }
