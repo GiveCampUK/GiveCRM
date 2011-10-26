368b79845494db8cfa3b2015b4c40a020c5549c0
stack72 <paul@paulstack.co.uk>
2011-10-26 14:57:21 +0100

    addition of the beginnings of a search service - this is currently only used in the CampaignsController

---
+++ src/GiveCRM.Web/Services/SearchService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GiveCRM.DataAccess;
+using GiveCRM.Models;
+using GiveCRM.Models.Search;
+
+namespace GiveCRM.Web.Services
+{
+    public class SearchService: ISearchService
+    {
+        private Search searchRepo = new Search();
+
+        public IEnumerable<Member> RunCampaign(int campaignId)
+        {
+            return searchRepo.RunCampaign(campaignId);
+        }
+
+        public IEnumerable<SearchCriteria> GetEmptySearchCriteria()
+        {
+            return searchRepo.GetEmptySearchCriteria();
+        }
+    }
+
+    public interface ISearchService
+    {
+        IEnumerable<Member> RunCampaign(int campaignId);
+        IEnumerable<SearchCriteria> GetEmptySearchCriteria();
+    }
+}
