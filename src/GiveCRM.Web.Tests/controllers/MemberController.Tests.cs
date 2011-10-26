6596df9a519b67d18568cd91da217458c26be991
Paul Stack <paul@paulstack.co.uk>
2011-10-26 19:26:27 +0100

    addition of tests to start to cover the MemberController

---
+++ src/GiveCRM.Web.Tests/controllers/MemberController.Tests.cs
@@ -0,0 +1,68 @@
+﻿using GiveCRM.Models;
+using GiveCRM.Web.Controllers;
+using GiveCRM.Web.Models.Members;
+using GiveCRM.Web.Services;
+using MvcContrib.TestHelper;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace GiveCRM.Web.Tests.controllers
+{
+    [TestFixture]
+    public class MemberControllerTests
+    {
+        private IDonationsService _donationsService;
+        private IMemberService _memberService;
+        private ICampaignService _campaignService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _campaignService = Substitute.For<ICampaignService>();
+            _donationsService = Substitute.For<IDonationsService>();
+            _memberService = Substitute.For<IMemberService>();
+        }
+
+        [Test]
+        public void Index_Action_Returns_View()
+        {
+            var controller = new MemberController(_donationsService, _memberService, _campaignService);
+            var result = controller.Index();
+
+            result.AssertViewRendered();
+        }
+
+        [Test]
+        public void Add_Action_Returns_View_With_ViewModel()
+        {
+            var controller = new MemberController(_donationsService, _memberService, _campaignService);
+            var result = controller.Add();
+
+            result.AssertViewRendered().WithViewData<MemberEditViewModel>();
+        }
+
+        [Test]
+        public void Edit_Action_Returns_Named_View_With_ViewModel()
+        {
+            _memberService.Get(1).Returns(new Member());
+
+            var controller = new MemberController(_donationsService, _memberService, _campaignService);
+            var result = controller.Edit(1);
+
+            Assert.That(result.AssertViewRendered().ViewName == "Add");
+            result.AssertViewRendered().WithViewData<MemberEditViewModel>();
+        }
+
+        [Test]
+        public void Delete_Action_Returns_RedirectToAction()
+        {
+            _memberService.Get(1).Returns(new Member());
+            _memberService.Update(new Member());
+
+            var controller = new MemberController(_donationsService, _memberService, _campaignService);
+            var result = controller.Delete(1);
+
+            result.AssertActionRedirect().ToAction("Index");
+        }
+    }
+}
