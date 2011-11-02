ff065ce7e9e181bf963e0ecf46c05163d3ba273f
Alastair Smith <github@alastairsmith.me.uk>
2011-11-02 00:13:54 +0000

    Add IPagedList<T> interface and implementation.

---
+++ src/GiveCRM.Web/Infrastructure/PagedList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GiveCRM.Web.Infrastructure
+{
+    /// <summary>
+    /// Defines the default implementation of <see cref="IPagedList"/>. 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PagedList<T> : List<T>, IPagedList<T>
+    {
+        public PagedList(IEnumerable<T> items, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            ItemsPerPage = pageSize;
+
+            var itemsList = items.ToList();
+            ItemCount = itemsList.Count;
+            AddRange(itemsList.Skip(pageIndex * pageSize).Take(pageSize).ToList());
+        }
+
+        public int ItemsPerPage { get; set; }
+        public int PageIndex { get; set; }
+        public int ItemCount { get; set; }
+
+        public bool HasPreviousPage
+        {
+            get { return (PageIndex > 0); }
+        }
+
+        public bool HasNextPage
+        {
+            get { return (PageIndex*ItemsPerPage) <= ItemCount; }
+        }
+    }
+}
