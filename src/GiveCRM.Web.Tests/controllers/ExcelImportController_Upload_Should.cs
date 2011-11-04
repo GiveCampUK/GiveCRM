4d78d369d98758d156fcd89f10ab6880cc3076ea
Alastair Smith <github@alastairsmith.me.uk>
2011-11-04 22:46:36 +0000

    Move ExcelImportCOntroller Upload tests to GiveCRM.Web.Tests - they'd been missed when I moved this stuff over from GiveCRM.Admin.  Also remove the Moq dependency from GiveCRM.Web.Tests as we're using NSubstitute instead.

---
+++ src/GiveCRM.Web.Tests/controllers/ExcelImportController_Upload_Should.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GiveCRM.Web.Controllers;
+using GiveCRM.Web.Services.ExcelImport;
+using NSubstitute;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace GiveCRM.Admin.Web.Tests
+{
+    [TestFixture]
+    public class ExcelImportController_Upload_Should
+    {
+        private ExcelImportController controller;
+        
+        private const string MemberControllerName = "Member";
+        private const string IndexActionName = "Index";
+
+        private const int ZeroFileLength = 0;
+        private const int NonZeroFileLength = 4096;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var excelImporter = Substitute.For<IExcelImportService>();
+            controller = new ExcelImportController(excelImporter);
+        }
+
+        [Test]
+        public void ReturnToIndexView_WhenNoFileIsSelectedForUpload()
+        {
+            var result = controller.ImportAsync(null) as ViewResult;
+            Assert.AreEqual(IndexActionName, result.ViewName);
+        }
+
+        [Test]
+        public void DisplayAnErrorMesage_WhenNoFileIsUploaded()
+        {
+            var result = controller.ImportAsync(null) as ViewResult;
+            Assert.IsNotNull(result.ViewBag.Error);
+        }
+
+        [Test]
+        public void ReturnToIndexView_WhenAnEmptyFileIsUploaded()
+        {
+            var file = Substitute.For<HttpPostedFileBase>();
+            file.ContentLength.Returns(ZeroFileLength);
+
+            var result = controller.ImportAsync(file) as ViewResult;
+
+            Assert.AreEqual(IndexActionName, result.ViewName);
+        }
+
+        [Test]
+        public void DisplayAnErrorMessage_WhenAnEmptyFileIsUploaded()
+        {
+            var file = Substitute.For<HttpPostedFileBase>();
+            file.ContentLength.Returns(ZeroFileLength);
+
+            var result = controller.ImportAsync(file) as ViewResult;
+
+            Assert.IsNotNull(result.ViewBag.Error);
+        }
+
+        [Test]
+        public void ReturnToIndexView_WhenANonExcelFileIsUploaded()
+        {
+            var file = Substitute.For<HttpPostedFileBase>();
+            file.ContentLength.Returns(NonZeroFileLength);
+            file.FileName.Returns("A Text File.txt");
+
+            var result = controller.ImportAsync(file) as ViewResult;
+
+            Assert.AreEqual(IndexActionName, result.ViewName);
+        }
+
+        [Test]
+        public void DisplayAnErrorMessage_WhenANonExcelFileIsUploaded()
+        {
+            var file = Substitute.For<HttpPostedFileBase>();
+            file.ContentLength.Returns(NonZeroFileLength);
+            file.FileName.Returns("A Text File.txt");
+
+            var result = controller.ImportAsync(file) as ViewResult;
+
+            Assert.IsNotNull(result.ViewBag.Error);
+        }
+
+        [Test]
+        public void NotReturnAViewResult_WhenAnExcelFileIsUploaded_OldFormat()
+        {
+            var file = Substitute.For<HttpPostedFileBase>();
+            file.ContentLength.Returns(NonZeroFileLength);
+            file.FileName.Returns("An old-format Excel file.xls");
+
+            var result = controller.ImportAsync(file);
+
+            Assert.IsNotInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public void NotReturnAViewResult_WhenAnExcelFileIsUploaded_NewFormat()
+        {
+            var file = Substitute.For<HttpPostedFileBase>();
+            file.ContentLength.Returns(NonZeroFileLength);
+            file.FileName.Returns("A new-format Excel file.xlsx");
+
+            var result = controller.ImportAsync(file);
+
+            Assert.IsNotInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public void RedirectToDashboardIndex_WhenFileIsValidForImport()
+        {
+            var file = Substitute.For<HttpPostedFileBase>();
+            file.ContentLength.Returns(NonZeroFileLength);
+            file.FileName.Returns("Valid file.xls");
+
+            var result = controller.ImportAsync(file) as RedirectToRouteResult;
+
+            Assert.AreEqual(MemberControllerName, result.RouteValues["controller"]);
+            Assert.AreEqual(IndexActionName, result.RouteValues["action"]);
+        }
+
+        [Test]
+        public void RedirectToDashboardIndex_WhenFileImportIsComplete()
+        {
+            var result = controller.ImportCompleted(Enumerable.Empty<IDictionary<string, object>>()) as RedirectToRouteResult;
+
+            Assert.AreEqual(MemberControllerName, result.RouteValues["controller"]);
+            Assert.AreEqual(IndexActionName, result.RouteValues["action"]);
+        }
+    }
+}
