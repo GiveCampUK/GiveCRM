7527c4622be0546723bc82c1ecbb888bb96ca136
Paul Stack <paul@paulstack.co.uk>
2011-11-04 22:13:06 +0000

    more work on the deployment script

--- build/run.ps1
+++ build/run.ps1
@@ -1,9 +1,14 @@
-param (
-	$Configuration = ''
-)
-
 import-module .\psake\psake.psm1
 
-invoke-psake ./build.ps1
+properties {
+	$Configuration = "release"
+	$run_type = "build"
+}
+
+switch($run_type)
+{
+	"deploy" { Invoke-psake ./deloy.ps1 }
+	default { Invoke-psake ./build.ps1 $Configuration }
+}
 
 remove-module psake
