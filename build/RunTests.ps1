a70735921fe78001a1552d694ad6343296937909
Saqib Shaikh <Me@SaqibShaikh.com>
2011-11-09 07:46:22 +0000

    Updating RunTests.ps1 to test each assembly individually, rather than the solution in one go - the latter does not work with app.config's for tests.

--- build/RunTests.ps1
+++ build/RunTests.ps1
@@ -1,6 +1,18 @@
 $root = ".."
+$configuration = "Release"
 
 $nunitDir = dir $root\src\packages\nunit*
 if ($nunitDir -eq $null) { "Could not find NUnit"; exit }
 
-&$nunitDir\tools\nunit-console.exe /nologo $root\src\GiveCRM.sln
+# The easy way was to test the solution, but this doesn't work with app.config's.
+# &$nunitDir\tools\nunit-console.exe /nologo /config:$configuration $root\src\GiveCRM.sln
+
+# Because of the app.config files, we find all test assemblies and test them individually.
+$testProjects = dir $root\src -recurse -include *.csproj | where { (type $_.FullName) -match "nunit" }
+foreach ($proj in $testProjects)
+{
+$assembly = "$($proj.DirectoryName)\bin\$configuration\$($proj.BaseName).dll"
+Write-Output "Testing $($proj.BaseName)"
+&$nunitDir\tools\nunit-console.exe /nologo $assembly
+Write-Output " "
+}
