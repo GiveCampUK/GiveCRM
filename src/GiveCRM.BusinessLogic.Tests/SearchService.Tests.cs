4435ee862b6948cfff2d4c87c316ee4e3eacee0d
Alastair Smith <github@alastairsmith.me.uk>
2011-11-11 23:17:07 +0000

    Fix failing SearchService test.

--- src/GiveCRM.BusinessLogic.Tests/SearchService.Tests.cs
+++ src/GiveCRM.BusinessLogic.Tests/SearchService.Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using GiveCRM.Models;
@@ -35,7 +36,7 @@ namespace GiveCRM.BusinessLogic.Tests
             var searchService = new SearchService(facetRepository);
             var actualEmptySearchCriteria = searchService.GetEmptySearchCriteria();
 
-            CollectionAssert.AreEqual(expectedEmptySearchCriteria.ToArray(), actualEmptySearchCriteria.ToArray());
+            CollectionAssert.AreEqual(expectedEmptySearchCriteria.ToArray(), actualEmptySearchCriteria.ToArray(), new SearchCriteriaComparer());
         }
 
         private static IEnumerable<SearchCriteria> GetExpectedEmptySearchCriteria(IEnumerable<Facet> freeTextFacets)
@@ -115,6 +116,66 @@ namespace GiveCRM.BusinessLogic.Tests
             facetRepository.GetAllFreeText().Returns(new[] { bigFeetFacet, smellyFeetFacet });
 
             return facetRepository;
+        }
+
+        private class SearchCriteriaComparer : IComparer, IComparer<SearchCriteria>
+        {
+            #region Implementation of IComparer<in SearchCriteria>
+
+            /// <summary>
+            /// Compares two objects and returns a value indicating whether one is less than, equal to, or greater than the other.
+            /// </summary>
+            /// <returns>
+            /// A signed integer that indicates the relative values of <paramref name="x"/> and <paramref name="y"/>, as shown in the following table.Value Meaning Less than zero<paramref name="x"/> is less than <paramref name="y"/>.Zero<paramref name="x"/> equals <paramref name="y"/>.Greater than zero<paramref name="x"/> is greater than <paramref name="y"/>.
+            /// </returns>
+            /// <param name="x">The first object to compare.</param><param name="y">The second object to compare.</param>
+            public int Compare(SearchCriteria x, SearchCriteria y)
+            {
+                int internalNameComparison = Comparer<string>.Default.Compare(x.InternalName, y.InternalName);
+                if (internalNameComparison != 0)
+                {
+                    return internalNameComparison;
+                }
+
+                int displayNameComparison = Comparer<string>.Default.Compare(x.DisplayName, y.DisplayName);
+                if (displayNameComparison != 0)
+                {
+                    return displayNameComparison;
+                }
+
+                return Comparer<SearchFieldType>.Default.Compare(x.Type, y.Type);
+            }
+
+            #endregion
+
+            #region Implementation of IComparer
+
+            /// <summary>
+            /// Compares two objects and returns a value indicating whether one is less than, equal to, or greater than the other.
+            /// </summary>
+            /// <returns>
+            /// A signed integer that indicates the relative values of <paramref name="x"/> and <paramref name="y"/>, as shown in the following table.Value Meaning Less than zero <paramref name="x"/> is less than <paramref name="y"/>. Zero <paramref name="x"/> equals <paramref name="y"/>. Greater than zero <paramref name="x"/> is greater than <paramref name="y"/>. 
+            /// </returns>
+            /// <param name="x">The first object to compare. </param><param name="y">The second object to compare. </param><exception cref="T:System.ArgumentException">Neither <paramref name="x"/> nor <paramref name="y"/> implements the <see cref="T:System.IComparable"/> interface.-or- <paramref name="x"/> and <paramref name="y"/> are of different types and neither one can handle comparisons with the other. </exception><filterpriority>2</filterpriority>
+            public int Compare(object x, object y)
+            {
+                var xSearchCriteria = x as SearchCriteria;
+                var ySearchCriteria = y as SearchCriteria;
+
+                if (xSearchCriteria == null && ySearchCriteria != null)
+                {
+                    return -1;
+                }
+
+                if (xSearchCriteria != null && ySearchCriteria == null)
+                {
+                    return 1;
+                }
+
+                return Compare(xSearchCriteria, ySearchCriteria);
+            }
+
+            #endregion
         }
     }
 }
