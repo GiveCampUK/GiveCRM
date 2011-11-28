3f53384fd117b95820131548b1a352628a43850d
Alastair Smith <github@alastairsmith.me.uk>
2011-11-28 20:40:59 +0000

    Fix whitespace in ExcelImportController

--- src/GiveCRM.Web/Controllers/ExcelImportController.cs
+++ src/GiveCRM.Web/Controllers/ExcelImportController.cs
@@ -6,7 +6,7 @@
     using System.Threading.Tasks;
     using System.Web;
     using System.Web.Mvc;
-	using GiveCRM.BusinessLogic.ExcelImport;
+    using GiveCRM.BusinessLogic.ExcelImport;
     using GiveCRM.Web.Models;
 
     public class ExcelImportController : AsyncController
