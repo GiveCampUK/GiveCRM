ab0e4244be9247d51ed2543724dcc95846e48df2
kuangmarkeleven <kuangmarkeleven@gmail.com>
2011-12-21 10:39:39 +0000

    added elmah script for sql setup and configured elmah to record in db

--- src/GiveCRM.Database/Scripts/Post-Deployment/Script.PostDeployment.sql
+++ src/GiveCRM.Database/Scripts/Post-Deployment/Script.PostDeployment.sql
@@ -9,3 +9,5 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+
+:r ELMAH.sql
