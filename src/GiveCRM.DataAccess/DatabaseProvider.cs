1ac3495706be935e7c13274bbf37c535f6be306b
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-03-29 23:37:56 +0100

    Added IDatabaseProvider and implementation to provide a component resolvable from the IoC container that means the Simple.Data dynamic database object can be re-used within a single web request.

---
+++ src/GiveCRM.DataAccess/DatabaseProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace GiveCRM.DataAccess
+{
+    using Simple.Data;
+
+    public class DatabaseProvider : IDatabaseProvider
+    {
+        private readonly dynamic database = Database.OpenNamedConnection("GiveCRM");
+
+        public dynamic GetDatabase()
+        {
+            return database;
+        }
+    }
+}
