41e114522a81bb0dfad54bfd973a927c35ccbcef
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-08-07 21:40:46 +0100

    Added xml comment as requested for DomainException.

--- src/GiveCRM.BusinessLogic/DomainException.cs
+++ src/GiveCRM.BusinessLogic/DomainException.cs
@@ -3,6 +3,10 @@
     using System;
     using System.Runtime.Serialization;
 
+    /// <summary>
+    /// An exception to be deliberatley thrown when exceptional circumstances arise in 
+    /// the domain use-cases, as opposed to more "technical" or unexpected exceptions.
+    /// </summary>
     [Serializable]
     public class DomainException : Exception
     {
