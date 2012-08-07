084f2e6806eb49883eae3a75658d31eeb72844e9
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-08-07 22:09:49 +0100

    Implemented TenantNotFoundException as InvalidOperationException doesn't really suit.

---
+++ src/GiveCRM.DataAccess/TenantNotFoundException.cs
@@ -0,0 +1,27 @@
+﻿namespace GiveCRM.DataAccess
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class TenantNotFoundException : Exception
+    {
+        public TenantNotFoundException()
+        {
+        }
+
+        public TenantNotFoundException(string message) : base(message)
+        {
+        }
+
+        public TenantNotFoundException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected TenantNotFoundException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
