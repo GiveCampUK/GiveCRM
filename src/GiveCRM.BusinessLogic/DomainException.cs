e881e77d0527253721be9df67340f965865173d0
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-08-07 21:37:52 +0100

    Renamed exception to DomainException.
    Also add xml comment.

---
+++ src/GiveCRM.BusinessLogic/DomainException.cs
@@ -0,0 +1,31 @@
+﻿namespace GiveCRM.BusinessLogic
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// An exception to be deliberatley thrown when exceptional circumstances arise in 
+    /// the domain use-cases, as opposed to more "technical" or unexpected exceptions.
+    /// </summary>
+    [Serializable]
+    public class DomainException : Exception
+    {
+        public DomainException()
+        {
+        }
+
+        public DomainException(string message) : base(message)
+        {
+        }
+
+        public DomainException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected DomainException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
